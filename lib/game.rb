fe360db184bcfc1c4c2798c887c6d9f8a7d572ae
harmolipi <nikob381@gmail.com>
2021-03-18 13:31:31 -0400

    Finished basic hangman functionality and split into separate game file

---
+++ lib/game.rb
@@ -0,0 +1,97 @@
+class Game
+  GUESS_PATTERN = /\b[a-zA-Z]\b|\bsave\b/
+  @secret_array = []
+  @correct_guesses = []
+  @progress = []
+  @incorrect_guesses = 10
+
+  def self.reset(secret_word)
+    @correct_guesses = Array.new(secret_word.length, '_')
+    @progress = []
+    @secret_array = secret_word.split('')
+    @incorrect_guesses = 10
+  end
+
+  def self.game_loop(secret_word)
+    Game.reset(secret_word)
+    guesses = []
+    puts secret_word
+    Game.display(guesses)
+    while @incorrect_guesses > 0
+      guess = Game.input_guess(guesses)
+      # guesses << (correct?(guess) ? guess.green : guess.gray)
+      if correct?(guess)
+        guesses << guess.green
+        Game.add_guesses(guess)
+      else
+        guesses << guess.magenta
+        @incorrect_guesses -= 1
+      end
+      Game.display(guesses)
+      won = Game.won?(secret_word)
+      break if won
+      # guesses << input_guess(guesses)
+    end
+    puts Game.ending(won, secret_word)
+  end
+
+  def self.input_guess(guesses)
+    begin
+      print "Enter a letter, or type 'save' to save your progress: "
+      guess = gets.chomp.downcase
+      raise 'Invalid guess, please try again.' unless guess.match?(GUESS_PATTERN)
+      raise 'You already guessed that letter!' if guesses.include?(guess.green) || guesses.include?(guess.magenta)
+    rescue StandardError => e
+      puts e.to_s.red
+      Game.display(guesses)
+      retry
+    end
+    guess
+  end
+
+  def self.display(guesses)
+    puts "#{@incorrect_guesses} incorrect guesses left."
+    puts ''
+    puts "#{Game.display_guesses(guesses)}"
+    puts ''
+    puts "#{Game.display_word}\n"
+    puts ''
+  end
+
+  def self.display_word
+    @progress = @correct_guesses.map do |letter|
+      letter.nil? ? '_' : letter
+    end
+    print @progress.join(' ')
+  end
+
+  def self.display_guesses(guesses)
+    print guesses.join(' ')
+  end
+
+  def self.add_guesses(guess)
+    @secret_array.each_with_index do |letter, index|
+      if letter == guess
+        @correct_guesses[index] = guess
+        @secret_array[index] = nil
+      end
+    end
+  end
+
+  def self.correct?(guess)
+    @secret_array.include?(guess)
+  end
+
+  def self.won?(secret_word)
+    # binding.pry
+    @correct_guesses.eql?(secret_word.split(''))
+  end
+
+  def self.ending(won, secret_word)
+    if won
+      "Well done, old chap! You've done it again!\n".green
+    else
+      "So sorry, old sport - you've failed! The word was '#{secret_word}'.\n".blue
+    end
+  end
+end
--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,37 +1,107 @@
 require_relative 'color'
+require_relative 'game'
+require 'pry'
 
 REPLAY_PATTERN = /\b(?:y|n|load)\b/
-GUESS_PATTERN = /\b[a-zA-Z]\b/
 play_game = true
 dictionary_words = File.readlines('5desk.txt').select { |word| word.strip.length.between?(5, 12) }
 
-def game_loop(secret_word)
-  guesses = []
-  incorrect_guesses = 10
-  while incorrect_guesses >= 0
-    begin
-      puts "Enter a letter, or type 'save' to save your progress: "
-      guess = gets.chomp
-      raise 'Invalid guess, please try again.' unless guess.match?(GUESS_PATTERN)
+# def reset(secret_word)
+#   @correct_guesses = Array.new(secret_word.length, '_')
+#   @progress = []
+#   @secret_array = secret_word.split('')
+#   @incorrect_guesses = 10
+# end
 
-      raise 'You already guessed that letter!' if guesses.include?(guess)
-    rescue StandardError => e
-      puts e.to_s.red
-      retry
-    end
-    guesses << guess
-    incorrect_guesses -= 1
-  end
-end
+# def game_loop(secret_word)
+#   reset(secret_word)
+#   guesses = []
+#   puts secret_word
+#   display(guesses)
+#   while @incorrect_guesses > 0
+#     guess = input_guess(guesses)
+#     # guesses << (correct?(guess) ? guess.green : guess.gray)
+#     if correct?(guess)
+#       guesses << guess.green
+#       add_guesses(guess)
+#     else
+#       guesses << guess.magenta
+#       @incorrect_guesses -= 1
+#     end
+#     display(guesses)
+#     won = won?(secret_word)
+#     break if won
+#     # guesses << input_guess(guesses)
+#   end
+
+#   ending(won)
+  
+# end
+
+# def input_guess(guesses)
+#   begin
+#     print "Enter a letter, or type 'save' to save your progress: "
+#     guess = gets.chomp.downcase
+#     raise 'Invalid guess, please try again.' unless guess.match?(GUESS_PATTERN)
+#     raise 'You already guessed that letter!' if guesses.include?(guess.green) || guesses.include?(guess.magenta)
+#   rescue StandardError => e
+#     puts e.to_s.red
+#     display(guesses)
+#     retry
+#   end
+#   guess
+# end
+
+# def display(guesses)
+#   puts "#{@incorrect_guesses} incorrect guesses left."
+#   puts ''
+#   puts "#{display_guesses(guesses)}"
+#   puts ''
+#   puts "#{display_word}\n"
+#   puts ''
+# end
+
+# def display_word
+#   @progress = @correct_guesses.map do |letter|
+#     letter.nil? ? '_' : letter
+#   end
+#   print @progress.join(' ')
+# end
+
+# def display_guesses(guesses)
+#   print guesses.join(' ')
+# end
+
+# def add_guesses(guess)
+#   @secret_array.each_with_index do |letter, index|
+#     if letter == guess
+#       @correct_guesses[index] = guess
+#       @secret_array[index] = nil
+#     end
+#   end
+# end
+
+# def correct?(guess)
+#   @secret_array.include?(guess)
+# end
+
+# def won?(secret_word)
+#     # binding.pry
+#     @correct_guesses.eql?(secret_word.split(''))
+# end
+
+# def ending(won)
+#   puts won ? "Well done, old chap! You've done it again!\n".green : "So sorry, old sport - you've failed!\n".blue
+# end
 
 puts 'Hangman Initialized'
 
 while play_game
-  secret_word = dictionary_words.sample
-  puts 'Time for a new hangman game!'
-  game_loop(secret_word)
+  secret_word = dictionary_words.sample.strip.downcase
+  puts "\nTime for a new hangman game!"
+  Game.game_loop(secret_word)
   begin
-    puts 'Would you like to play again? (y/n) '
+    print 'Would you like to play again? (y/n) '
     answer = gets.chomp
     raise unless answer.match?(REPLAY_PATTERN)
   rescue StandardError
@@ -41,8 +111,17 @@ while play_game
   play_game = answer == 'y'
 end
 
-# _ _ _ _ _ _ _ _
-#
-# Letters guessed:
-#
-#Enter your guess (x guesses left): 
+# while play_game
+#   secret_word = dictionary_words.sample.strip.downcase
+#   puts "\nTime for a new hangman game!"
+#   game_loop(secret_word)
+#   begin
+#     print 'Would you like to play again? (y/n) '
+#     answer = gets.chomp
+#     raise unless answer.match?(REPLAY_PATTERN)
+#   rescue StandardError
+#     puts 'Invalid entry, try again.'.red
+#     retry
+#   end
+#   play_game = answer == 'y'
+# end
